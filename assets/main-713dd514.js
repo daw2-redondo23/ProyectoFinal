563fa86fc2bf5142516f9e391370067fa3e29059
daw2-redondo23 <redondobarrosoruben@fpllefia.com>
2023-04-19 16:11:59 +0200

    Updates

---
+++ assets/main-713dd514.js
@@ -0,0 +1,60 @@
+(function polyfill() {
+  const relList = document.createElement("link").relList;
+  if (relList && relList.supports && relList.supports("modulepreload")) {
+    return;
+  }
+  for (const link of document.querySelectorAll('link[rel="modulepreload"]')) {
+    processPreload(link);
+  }
+  new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      if (mutation.type !== "childList") {
+        continue;
+      }
+      for (const node of mutation.addedNodes) {
+        if (node.tagName === "LINK" && node.rel === "modulepreload")
+          processPreload(node);
+      }
+    }
+  }).observe(document, { childList: true, subtree: true });
+  function getFetchOpts(link) {
+    const fetchOpts = {};
+    if (link.integrity)
+      fetchOpts.integrity = link.integrity;
+    if (link.referrerPolicy)
+      fetchOpts.referrerPolicy = link.referrerPolicy;
+    if (link.crossOrigin === "use-credentials")
+      fetchOpts.credentials = "include";
+    else if (link.crossOrigin === "anonymous")
+      fetchOpts.credentials = "omit";
+    else
+      fetchOpts.credentials = "same-origin";
+    return fetchOpts;
+  }
+  function processPreload(link) {
+    if (link.ep)
+      return;
+    link.ep = true;
+    const fetchOpts = getFetchOpts(link);
+    fetch(link.href, fetchOpts);
+  }
+})();
+const style = "";
+const footer = {
+  template: `
+        <h5>Esto sera el footer</h5>
+    `
+};
+const header = {
+  template: `<h5>esto es el header</h5>
+        
+    `
+};
+const home = {
+  template: `<h1>esta sera la pagina home del proyecto</h1>
+        
+    `
+};
+document.querySelector("header").innerHTML = header.template;
+document.querySelector("main").innerHTML = home.template;
+document.querySelector("footer").innerHTML = footer.template;
